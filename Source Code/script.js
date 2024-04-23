d2bcef0e2b27d8edbd91a6e7dd59ce47df060907
S0ndr3 <sondrehaugejohnsrud@gmail.com>
2024-04-23 18:21:10 +0200

    Pray to god this works

--- Source Code/script.js
+++ Source Code/script.js
@@ -1,3 +1,12 @@
 function showPopUp() {
     document.getElementById("popup").style.visibility = "visible";
 }
+
+function showBurger() {
+    var x = document.getElementById("burgerLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+} 
